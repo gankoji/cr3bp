a67b0b784782db5995441a74547987c1d9466d03
Jake Bailey <asaxplayinghorse@gmail.com>
2018-12-10 00:37:42 -0700

    Event function updated. Geocentric plot added.

--- PythonCode/Main_KS.py
+++ PythonCode/Main_KS.py
@@ -17,9 +17,9 @@ r_e0 = np.array([smaEarth, 0, 0]) # km, Earth's average orbital
 v_e0 = np.array([0, smaEarth*wEarth, 0]) # km/s, Earth's average orbital velocity
                              # in the y inertial direction
 
-r_sc0 = r_e0 + np.array([6.5e3, 0, 0]) # km, Spacecraft's initial
+r_sc0 = r_e0 + np.array([6.6e3, 0, 0]) # km, Spacecraft's initial
                                        # position added to Earth's
-v_sc0 = v_e0 + np.array([1, 4.0, 0]) # km/s, Spacecraft's initial
+v_sc0 = v_e0 + np.array([0, 8.0, 0]) # km/s, Spacecraft's initial
                                        # velocity added to Earth's
 
 # Unit Conversion
@@ -32,16 +32,20 @@ debug = False
 
 if debug:
     simLength = 400*secondsPerDay
+    endData = simLength
     dt = 900
     simLength = simLength/DU
     dt = dt/DU
 else:
-    simLength = 350*secondsPerDay
-    dt = 300
+    simLength = 365*secondsPerDay
+    endDate = simLength
+    dt = 600
     simLength = simLength/DU
     dt = dt/DU
     
-N= int(math.floor(simLength/dt))
+J = int(math.floor(simLength/dt))
+print(J)
+N = int(1e6)
 simTime = np.linspace(0,simLength, N)
 flag = 1
 relerr = 1e-8
@@ -50,6 +54,7 @@ abserr = 1e-6
 # Sim output storage
 y_out = np.zeros((N,10))
 x_out = np.zeros((N,3))
+x_gc_out = np.zeros((N,3))
 e_out = np.zeros((N,3))
 H_out = np.zeros((N,))
 
@@ -58,6 +63,7 @@ v_sc0 = v_sc0/(DU*TU)
 
 x_out[0,:] = r_sc0
 e_out[0,:] = (smaEarth/DU)*np.array([1,0,0])
+x_gc_out[0, :] = x_out[0, :] - e_out[0, :]
 
 u0 = ks_init(r_sc0, v_sc0, 0, mu_s, 0)
 
@@ -65,11 +71,14 @@ y_out[0,:] = u0
 yp = np.zeros((10,))
 Hrel = np.zeros((N,))
 
-for i,x in enumerate(simTime):
+exitFlag = True
+i = 0
+while exitFlag:
     if i==0:
         H_out[i] = u0[8]
         Hrel[i] = 0
     else:
+        x = simTime[i]
         y = np.reshape(y_out[i-1,:],(10,))
         t0 = simTime[i-1]
         y, yp, t, flag = r8_rkf45( dynamics_ks, 10,
@@ -83,15 +92,21 @@ for i,x in enumerate(simTime):
 
         p, pdot = ks2cart(y)
         x_out[i,:] = p
-        t = y[9]*np.linalg.norm(y[0:4])
+        t = y[9]
         lonEarth = wEarth*t/TU
         e_out[i,:] = (smaEarth/DU)*np.array([math.cos(lonEarth),
                                              math.sin(lonEarth), 0])
         ve = (smaEarth/(DU*TU))*wEarth*np.array([-math.sin(lonEarth),
-                                            math.cos(lonEarth), 0])
+                                                 math.cos(lonEarth), 0])
 
-        #print(np.linalg.norm(ve))
-        
+        if fabs(y[9] - endDate*TU) < 1e-3:
+            exitFlag = False
+            print("End Time")
+            print(i)
+        elif i >= N-1:
+            print(i)
+            exitFlag = False
+            
         if not inSoI:
             r_sc_e = p - e_out[i,:]
         else:
@@ -103,7 +118,6 @@ for i,x in enumerate(simTime):
                 print("Capturing")
                 inSoI = True
                 r_sc = r_sc_e
-                print(np.linalg.norm(r_sc))
                 v_sc = pdot - ve
                 y = ks_init(r_sc, v_sc, y[9], mu_e, 0)
                 y_out[i, :] = y
@@ -117,17 +131,45 @@ for i,x in enumerate(simTime):
                 print("Releasing")
                 inSoI = False
                 r_sc = p + e_out[i, :]
-                print(np.linalg.norm(r_sc))
                 v_sc = pdot + ve
                 y = ks_init(r_sc, v_sc, y[9], mu_s, 0)
                 yp = dynamics_ks(x, y)
                 y_out[i, :] = y
+                x_out[i, :] = p + e_out[i, :]
+
+    x_gc_out[i, :] = x_out[i, :] - e_out[i, :]
+    i = i + 1
+
+new_e_out = np.trim_zeros(e_out[:,0])
+a = new_e_out.size
+
+new_e_out = np.zeros((a, 2))
+new_e_out[:, 0] = np.trim_zeros(e_out[:,0])
+new_e_out[:, 1] = np.resize(np.trim_zeros(e_out[:,1]), (a,))
+new_e_out = new_e_out[:-1, :]
+
+new_x_out = np.zeros((a, 2))
+new_x_out[:, 0] = np.trim_zeros(x_out[:,0])
+new_x_out[:, 1] = np.resize(np.trim_zeros(x_out[:,1]), (a,))
+new_x_out = new_x_out[:-1, :]
+
+new_x_gc_out = np.zeros((a, 2))
+new_x_gc_out[:, 0] = np.trim_zeros(x_gc_out[:,0])
+new_x_gc_out[:, 1] = np.resize(np.trim_zeros(x_gc_out[:,1]), (a,))
+new_x_gc_out = new_x_gc_out[:-1, :]
 
 print("Finished: " + datetime.datetime.now().strftime("%H:%M:%S.%f"))
 
+fig2, ax2 = plt.subplots()
+ax2.plot(new_x_gc_out[:, 0], new_x_gc_out[:, 1])
+ax2.set(xlabel='X - AU',ylabel='Y - AU',
+       title='Geocentric Satellite Motion')
+ax2.legend(['Spacecraft'])
+fig2.savefig('GeoPlanarPath.png')
+
 fig, ax = plt.subplots()
-ax.plot(e_out[:,0], e_out[:,1])
-ax.plot(x_out[:,0], x_out[:,1])
+ax.plot(new_e_out[:,0], new_e_out[:,1])
+ax.plot(new_x_out[:,0], new_x_out[:,1])
 ax.plot([0.0],[0.0],'r+')
 ax.set(xlabel='X - AU',ylabel='Y - AU',
        title='Planar Motion of Two Bodies')
@@ -135,7 +177,7 @@ ax.legend(['Earth','Spacecraft','Sol'])
 fig.savefig('PlanarPath.png')
 
 fig1, ax1 = plt.subplots()
-ax1.plot(simTime/secondsPerDay*smaEarth, Hrel)
+ax1.plot(simTime/secondsPerDay*smaEarth, H_out)
 ax1.set(xlabel='Time (Days)',ylabel='Energy',
         title='Hamiltonian (Total Energy)')
 fig1.savefig('Hamiltonian.png')
